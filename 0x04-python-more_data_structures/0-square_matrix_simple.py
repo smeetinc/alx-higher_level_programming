52e6ea9183c0c08f1a27819772ab633665b94905
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2023-08-16 06:08:17 -0700

    Task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    n_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        n_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+        return (n_matrix)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,2 @@
+Tasks on more data structures, sets and dictionary Done on 8/16/2023
+by Oluwaseyi Smith Bayode
